2569303a6e17cf24a279a8b5c1dbdc2df8d5bf60
alaay9003 <alaay9003@gmail.com>
2023-04-25 22:10:35 +0200

    Add project files.

---
+++ kafka_test/Models/CarDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace kafka_test.Models
+{
+    public class CarDetails
+    {
+        public int CarId { get; set; }
+        public string CarName { get; set; }
+        public string BookingStatus { get; set; }
+    }
+
+}
